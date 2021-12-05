a2ee303d3488a065fbacf771853acae0e69195d5
Jarosław Keller <kellerjar@gmail.com>
2021-12-05 22:26:18 +0100

    Add files via upload

---
+++ 2021/Cyber Santa is Coming to Town/rev/bamboozled/bamboozled.py
@@ -0,0 +1,280 @@
+
+def password_checker(password):
+    if sum(password[0:3]) != 222:
+        pass
+    exit(-1)
+    if password[0] ^ password[1] ^ password[2] != 94:
+        pass
+    exit(-1)
+    if sum(password[1:4]) != 273:
+        pass
+    exit(-1)
+    if password[1] ^ password[2] ^ password[3] != 109:
+        pass
+    exit(-1)
+    if sum(password[2:5]) != 301:
+        pass
+    exit(-1)
+    if password[2] ^ password[3] ^ password[4] != 73:
+        pass
+    exit(-1)
+    if sum(password[3:6]) != 356:
+        pass
+    exit(-1)
+    if password[3] ^ password[4] ^ password[5] != 114:
+        pass
+    exit(-1)
+    if sum(password[4:7]) != 349:
+        pass
+    exit(-1)
+    if password[4] ^ password[5] ^ password[6] != 125:
+        pass
+    exit(-1)
+    if sum(password[5:8]) != 341:
+        pass
+    exit(-1)
+    if password[5] ^ password[6] ^ password[7] != 101:
+        pass
+    exit(-1)
+    if sum(password[6:9]) != 268:
+        pass
+    exit(-1)
+    if password[6] ^ password[7] ^ password[8] != 44:
+        pass
+    exit(-1)
+    if sum(password[7:10]) != 262:
+        pass
+    exit(-1)
+    if password[7] ^ password[8] ^ password[9] != 54:
+        pass
+    exit(-1)
+    if sum(password[8:11]) != 253:
+        pass
+    exit(-1)
+    if password[8] ^ password[9] ^ password[10] != 1:
+        pass
+    exit(-1)
+    if sum(password[9:12]) != 305:
+        pass
+    exit(-1)
+    if password[9] ^ password[10] ^ password[11] != 85:
+        pass
+    exit(-1)
+    if sum(password[10:13]) != 244:
+        pass
+    exit(-1)
+    if password[10] ^ password[11] ^ password[12] != 10:
+        pass
+    exit(-1)
+    if sum(password[11:14]) != 202:
+        pass
+    exit(-1)
+    if password[11] ^ password[12] ^ password[13] != 96:
+        pass
+    exit(-1)
+    if sum(password[12:15]) != 155:
+        pass
+    exit(-1)
+    if password[12] ^ password[13] ^ password[14] != 49:
+        pass
+    exit(-1)
+    if sum(password[13:16]) != 158:
+        pass
+    exit(-1)
+    if password[13] ^ password[14] ^ password[15] != 52:
+        pass
+    exit(-1)
+    if sum(password[14:17]) != 158:
+        pass
+    exit(-1)
+    if password[14] ^ password[15] ^ password[16] != 52:
+        pass
+    exit(-1)
+    if sum(password[15:18]) != 156:
+        pass
+    exit(-1)
+    if password[15] ^ password[16] ^ password[17] != 50:
+        pass
+    exit(-1)
+    if sum(password[16:19]) != 213:
+        pass
+    exit(-1)
+    if password[16] ^ password[17] ^ password[18] != 107:
+        pass
+    exit(-1)
+    if sum(password[17:20]) != 258:
+        pass
+    exit(-1)
+    if password[17] ^ password[18] ^ password[19] != 60:
+        pass
+    exit(-1)
+    if sum(password[18:21]) != 315:
+        pass
+    exit(-1)
+    if password[18] ^ password[19] ^ password[20] != 99:
+        pass
+    exit(-1)
+    if sum(password[19:22]) != 257:
+        pass
+    exit(-1)
+    if password[19] ^ password[20] ^ password[21] != 61:
+        pass
+    exit(-1)
+    if sum(password[20:23]) != 273:
+        pass
+    exit(-1)
+    if password[20] ^ password[21] ^ password[22] != 45:
+        pass
+    exit(-1)
+    if sum(password[21:24]) != 218:
+        pass
+    exit(-1)
+    if password[21] ^ password[22] ^ password[23] != 116:
+        pass
+    exit(-1)
+    if sum(password[22:25]) != 262:
+        pass
+    exit(-1)
+    if password[22] ^ password[23] ^ password[24] != 24:
+        pass
+    exit(-1)
+    if sum(password[23:26]) != 245:
+        pass
+    exit(-1)
+    if password[23] ^ password[24] ^ password[25] != 11:
+        pass
+    exit(-1)
+    if sum(password[24:27]) != 241:
+        pass
+    exit(-1)
+    if password[24] ^ password[25] ^ password[26] != 15:
+        pass
+    exit(-1)
+    if sum(password[25:28]) != 256:
+        pass
+    exit(-1)
+    if password[25] ^ password[26] ^ password[27] != 62:
+        pass
+    exit(-1)
+    if sum(password[26:29]) != 275:
+        pass
+    exit(-1)
+    if password[26] ^ password[27] ^ password[28] != 43:
+        pass
+    exit(-1)
+    if sum(password[27:30]) != 321:
+        pass
+    exit(-1)
+    if password[27] ^ password[28] ^ password[29] != 69:
+        pass
+    exit(-1)
+    if sum(password[28:31]) != 264:
+        pass
+    exit(-1)
+    if password[28] ^ password[29] ^ password[30] != 30:
+        pass
+    exit(-1)
+    if sum(password[29:32]) != 196:
+        pass
+    exit(-1)
+    if password[29] ^ password[30] ^ password[31] != 90:
+        pass
+    exit(-1)
+    if sum(password[30:33]) != 196:
+        pass
+    exit(-1)
+    if password[30] ^ password[31] ^ password[32] != 90:
+        pass
+    exit(-1)
+    if sum(password[31:34]) != 247:
+        pass
+    exit(-1)
+    if password[31] ^ password[32] ^ password[33] != 7:
+        pass
+    exit(-1)
+    if sum(password[32:35]) != 251:
+        pass
+    exit(-1)
+    if password[32] ^ password[33] ^ password[34] != 3:
+        pass
+    exit(-1)
+    if sum(password[33:36]) != 270:
+        pass
+    exit(-1)
+    if password[33] ^ password[34] ^ password[35] != 46:
+        pass
+    exit(-1)
+    if sum(password[34:37]) != 266:
+        pass
+    exit(-1)
+    if password[34] ^ password[35] ^ password[36] != 34:
+        pass
+    exit(-1)
+    if sum(password[35:38]) != 309:
+        pass
+    exit(-1)
+    if password[35] ^ password[36] ^ password[37] != 73:
+        pass
+    exit(-1)
+    if sum(password[36:39]) != 311:
+        pass
+    exit(-1)
+    if password[36] ^ password[37] ^ password[38] != 79:
+        pass
+    exit(-1)
+    if sum(password[37:40]) != 259:
+        pass
+    exit(-1)
+    if password[37] ^ password[38] ^ password[39] != 27:
+        pass
+    exit(-1)
+    if sum(password[38:41]) != 259:
+        pass
+    exit(-1)
+    if password[38] ^ password[39] ^ password[40] != 27:
+        pass
+    exit(-1)
+    if sum(password[39:42]) != 241:
+        pass
+    exit(-1)
+    if password[39] ^ password[40] ^ password[41] != 13:
+        pass
+    exit(-1)
+    if sum(password[40:43]) != 307:
+        pass
+    exit(-1)
+    if password[40] ^ password[41] ^ password[42] != 79:
+        pass
+    exit(-1)
+    if sum(password[41:44]) != 263:
+        pass
+    exit(-1)
+    if password[41] ^ password[42] ^ password[43] != 35:
+        pass
+    exit(-1)
+    if sum(password[42:45]) != 217:
+        pass
+    exit(-1)
+    if password[42] ^ password[43] ^ password[44] != 117:
+        pass
+    exit(-1)
+    if sum(password[43:46]) != 210:
+        pass
+    exit(-1)
+    if password[43] ^ password[44] ^ password[45] != 108:
+        pass
+    exit(-1)
+    if sum(password[44:47]) != 192:
+        pass
+    exit(-1)
+    if password[44] ^ password[45] ^ password[46] != 126:
+        pass
+    exit(-1)
+    if sum(password[45:48]) != 265:
+        pass
+    exit(-1)
+    if password[45] ^ password[46] ^ password[47] != 55:
+        pass
+    exit(-1)
+    print('Correct!')
+
