355e1dd650d859c1bebb19266e338695ad471829
Jarosław Keller <kellerjar@gmail.com>
2023-10-15 13:55:56 +0200

    solver.py

--- 2023/ISITDTU/rev/sanity_check/solver.py
+++ 2023/ISITDTU/rev/sanity_check/solver.py
@@ -1,14 +1,12 @@
 
 import gdb
 
-
 class HelloPy(gdb.Command):
     def __init__(self):
         super(HelloPy, self).__init__('hello_py', gdb.COMMAND_USER)
 
     def invoke(self, _unicode_args, _from_tty):
 
-        x = '0' * 23
         for _ in range(35):
             x = int(gdb.execute('x/gx $rsp+$rbx*8+0x58',
                     to_string=True).split()[-1], 16)
