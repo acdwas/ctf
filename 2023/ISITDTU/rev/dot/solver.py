ef4b220c3a0ed0f88cfcc5be8bf2442696b1573f
Jarosław Keller <kellerjar@gmail.com>
2023-10-15 14:53:25 +0200

    Add files via upload

---
+++ 2023/ISITDTU/rev/dot/solver.py
@@ -0,0 +1,11 @@
+
+l1 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+l2 = 'SED79K8AFHBC5GMQPJ6LU_OT4IV1W0ZXY32R'
+
+# ..... ... --- .. --.. ----- --... .---- --. ----- --.. -.-- --... _ ..... ... --- --...
+x = '5SOIZ071G0ZY7_T5SO7'
+
+for i in x:
+    print(l1[l2.index(i)], end='')
+
+# C0MPUT3RDTUW3LC0M3
